766877a1359d6f58f4ca98be1c928544bd5fb085
omnisonic <omnisonic@gmail.com>
2022-03-20 16:01:07 -0700

    ses settings

--- work_log/settings.py
+++ work_log/settings.py
@@ -147,8 +147,8 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = 'True'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USERNAME')
 EMAIL_HOST_PASSWORD =  os.environ.get('EMAIL_PASSWORD')
 
