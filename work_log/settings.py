60420ffa6a8a4234cd76bed4d10ecb26d80ac2d4
omnisonic <omnisonic@gmail.com>
2022-03-20 15:41:56 -0700

    mailtrap test2

--- work_log/settings.py
+++ work_log/settings.py
@@ -145,15 +145,14 @@ LOGIN_URL = 'login'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+
 # EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
 # EMAIL_PORT = '587'
 # EMAIL_USE_TLS = 'True'
-# # EMAIL_USE_SSL = True
 # EMAIL_HOST_USER = os.environ.get('EMAIL_USERNAME')
 # EMAIL_HOST_PASSWORD =  os.environ.get('EMAIL_PASSWORD')
-# SERVER_EMAIL = 'jctech@jctech.xyz'
-# DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = 'jctech@jctech.xyz'
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 EMAIL_HOST = 'smtp.mailtrap.io'
 EMAIL_HOST_USER = 'c960728220a834'
